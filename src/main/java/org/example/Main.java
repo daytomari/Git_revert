890a1b5a73a67f6812eba65dee3f8b32bf27437e
Tomari_Home <daytomari@gmail.com>
2024-11-05 00:07:34 +0900

    「しんごまま」追加

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Git練習を始めます");
         System.out.println("おっはー");
+        System.out.println("しんごまま");
 
     }
 }
