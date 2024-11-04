18e7f1c88a4aad55c11edc7a66ce421b5fb6c4f5
Tomari_Home <daytomari@gmail.com>
2024-11-05 00:13:18 +0900

    「高瀬さん」追加

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Git練習を始めます");
         System.out.println("安積さん参加");
+        System.out.println("高瀬さん参加");
     }
 }
