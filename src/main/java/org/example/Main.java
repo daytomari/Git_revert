da0533f7a3dd40299daa60fee9478e8089ff77e1
Tomari_Home <daytomari@gmail.com>
2024-11-05 00:11:08 +0900

    整理

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,11 +3,5 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Git練習を始めます");
-        System.out.println("おっはー");
-        System.out.println("しんごまま");
-        System.out.println("おっす");
-        System.out.println("おらご空");
-        System.out.println("練習中");
-
     }
 }
