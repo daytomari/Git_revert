11ef039503965e56dd45fe3d04e252207f1ee2e9
Tomari_Home <daytomari@gmail.com>
2024-11-05 00:03:45 +0900

    マージ後

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Git練習を始めます");
+        System.out.println("おはよう");
         System.out.println("こんにちは");
         System.out.println("こんばんは");
         System.out.println("練習をそろそろ終わる？");
